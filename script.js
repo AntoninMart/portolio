2bb9686c9da9d9ed2b7cc7ae2137c1363d3bf3b2
Antonin <85553842+AntoninMart@users.noreply.github.com>
2023-03-29 11:56:11 +0200

    add light / dark theme

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+let theme;
 
+window.onload = function () {
+    theme = localStorage.getItem("theme");
+    if (!theme) {
+        theme = "dark";
+        localStorage.setItem("theme", theme);
+    }
+    changeCss();
+}
 
-// document.addEventListener('DOMContentLoaded', function() {
-    //     // your code here
-    //     // if (document.body.style.backgroundColor == "#191919") {
-        //     //     document.getElementById("moon").style.display = "none";
-        //     //     document.getElementById("sun").style.display = "block";
-        //     // } else {
-            //     //     document.getElementById("sun").style.display = "none";
-            //     //     document.getElementById("moon").style.display = "block";
-            //     // }
-            //     console.log("DOM entièrement chargé et analysé");
-            //  }, false);
-            
-            // window.addEventListener("DOMContentLoaded", (event) => {
-                //     console.log("DOM entièrement chargé et analysé");
-                // });
-                
-document.getElementById("buttonLightMode").onclick = changeLight();
 function changeLight() {
-    // Change background color of body
-    document.body.style.backgroundColor = "red";
+    theme == "dark" ? theme = "light" : theme = "dark";
+    localStorage.setItem("theme", theme);
+    changeCss();
+}
+
+function changeCss() {
+    let darkLink = document.getElementById('dark-css');
+    let lightLink = document.getElementById('light-css');
+    if (theme == "light") {
+        darkLink.setAttribute('media', 'none');
+        lightLink.setAttribute('media', '');
+    } else {
+        lightLink.setAttribute('media', 'none');
+        darkLink.setAttribute('media', '');
+    }
 }
